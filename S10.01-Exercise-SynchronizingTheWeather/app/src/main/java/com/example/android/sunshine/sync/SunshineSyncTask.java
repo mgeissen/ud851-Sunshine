81c93a6dc68d4c10a8b0f6613f2e9c68ea889584
= <matt.geis97@gmail.com>
2017-11-26 13:33:21 +0100

    add first implementation

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -1,3 +1,12 @@
+package com.example.android.sunshine.sync;
+
+public class SunshineSyncTask{
+
+    synchronized public static void syncWeater(){
+
+    }
+
+}
 //  TODO (1) Create a class called SunshineSyncTask
 //  TODO (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
 //      TODO (3) Within syncWeather, fetch new weather data
